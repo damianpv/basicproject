1a4b7c717c935c33344be75b0731ae432973ac0b
Damian Perez <damian.adriel@gmail.com>
2015-08-20 10:49:11 -0400

    CHG: html template added

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='go_home'),
 
+    url(r'test1/$', views.Test1View.as_view(), name='go_test1'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -9,3 +9,12 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['header_menu'] = 'home'
         return context
+
+
+class Test1View(TemplateView):
+    template_name = 'test/index1.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Test1View, self).get_context_data(**kwargs)
+        context['header_menu'] = 'test'
+        return context
